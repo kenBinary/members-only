c386590816da009252495eb132638715375af6be
thennek0 <kennethbecaro0@gmail.com>
2023-08-11 18:30:49 +0800

    add signup controller

--- controllers/memberController.js
+++ controllers/memberController.js
@@ -2,9 +2,20 @@
 // -sign up new member
 // -create new message
 // -delete message
+
+// User model
 const User = require("../models/user");
 const asyncHandler = require("express-async-handler");
+// validation and sanitation
 const { check, validationResult } = require("express-validator");
+// authnetication 
+const passport = require("passport");
+const passportLocal = require("passport-local");
+const session = require("express-session");
+// password encrypting
+const bcrypt = require("bcrypt");
+// dotenv
+require('dotenv').config();
 
 
 
@@ -19,15 +30,26 @@ exports.loginValidation = [
 
 exports.login = asyncHandler(async (req, res, next) => {
     // login with authentication using passport
+    const errors = validationResult(req);
 });
-
 exports.signUp = asyncHandler(async (req, res, next) => {
     // sign-up with sanitation and validation whose password is stored with hash
-    const name = req.body.name;
-    const email = req.body.email;
-    const password = req.body.password;
-    const confirmedPassord = req.body.cPassword;
-    const code = req.body.secretCode;
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        throw new Error("Sign up validation and sanitation errors");
+    }
+    bcrypt.hash(req.body.password, 5, async (err, hash) => {
+        const code = (req.body.secretCode === process.env.admin_code);
+        const newUser = {
+            name: req.body.name,
+            email: req.body.email,
+            hash: hash,
+            isAdmin: code
+        }
+        const saveUser = new User(newUser);
+        await saveUser.save();
+    })
+    res.redirect("/");
 });
 
 // name should just be string with minimum 0 length
